e6da2d5c14b896243d137b1cf04c4bc8a1e8b6b0
Kinyua <alfkinyua@gmail.com>
2022-12-12 16:55:57 +0300

    Add methods to articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  
   root 'pages#index'
-  get 'about', to:'pages#about'
+  get 'about', to: 'pages#about'
   resources :articles
 
-
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
