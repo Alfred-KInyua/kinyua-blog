20a2060be77179ccb00ba76b363ccaf0159ce2fe
Kinyua <alfkinyua@gmail.com>
2022-12-17 16:32:56 +0300

    Add Edit action and template on Kinyuas blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,13 +5,30 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash.notice='Article was successfully updated'
+      redirect_to article_path(@article)
+    else
+      render :edit, status: :unprocessable_entity
+
+
+    end
+  
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = 'The article was saved successfully'
+      flash.notice = 'The article was saved successfully'
       redirect_to article_path(@article)
     else
-        render :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
