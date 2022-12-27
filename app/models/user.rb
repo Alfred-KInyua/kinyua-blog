745f8e5dac33fef59aea5377ba901f47e494aaa1
Kinyua <alfkinyua@gmail.com>
2022-12-27 19:00:26 +0300

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :username, presence: true, length:{ minimum: 3, maximum: 70 }
+    validates :email, presence: true, length:{ minimum: 3, maximum: 70 }
 end
