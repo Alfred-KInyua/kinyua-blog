08daf9bd54e43ca9dcbd19cbcf40f0a9b4dc2dd7
Kinyua <alfkinyua@gmail.com>
2022-12-29 09:31:00 +0300

    signup users from UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,12 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if 
         @user.save
-        flash[:notice]="User saved successfully"
-        redirect_to user_path(@user)
+        flash[:notice]="User saved successfully #{@user.username}"
+        redirect_to articles_path
     else
         render :new, status: :unprocessable_entity
   end
 end
-
   private
 
   def user_params
